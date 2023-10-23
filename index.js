22933235e4bb989f2066dfabdee8b94a861a3572
Zach Fountain <141088304+ZachFount@users.noreply.github.com>
2023-10-23 18:15:04 -0400

    adding 404 page view

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-  res.send('Hello world home page')
+  res.render('Hello world home page')
 })
 
 app.get('*', (req, res) => {
-  res.send('404 page')
+  res.render('404 page')
 })
 
 app.listen(process.env.PORT)
