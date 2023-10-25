2114d6eb1c938ce51cc3a843d34bfcf8a90a9c75
Zach Fountain <141088304+ZachFount@users.noreply.github.com>
2023-10-25 18:33:11 -0400

    adding home and 404 images

--- views/default.jsx
+++ views/default.jsx
@@ -9,7 +9,7 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
-                
+                <link rel="stylesheet" href="/css/style.css"/>
             </head>
             <body>
                 {html.children}
@@ -17,4 +17,5 @@ function Def (html) {
         </html>
     )
   }
+  
   
