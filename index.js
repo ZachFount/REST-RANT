adc31a7770c8893dcd729dbd7353b86029411eea
Zach Fountain <141088304+ZachFount@users.noreply.github.com>
2023-11-06 11:00:05 -0500

    json fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 
 const app = express()
 
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
 
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
